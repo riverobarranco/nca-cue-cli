739d8e18452370a907ba337a5e693a1fae888b24
Eduardo Rivero Barranco <riverobarranco@gmail.com>
2021-11-10 11:22:18 +0100

    ComponenteColapsa

---
+++ src/components/botoncolapsa.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="nca13_mnu_tglbtn" class="row" v-on:click="togleaestado()">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'botoncolapsa',
+  emits: ['toglea-estado'],
+  props: {
+    lateralvisible: {
+        type: Boolean,
+        default: true
+    },
+  },
+  computed: {
+    botonstyle() {
+        if (this.lateralvisible == true) {
+        return 'width: ' + (100 / this.botonessubtipos.length) + '%' + '; background-color: ';
+        } else {
+        return 'background-color: ';
+        }
+    },
+  },
+  methods: {
+    togleaestado() {
+        this.$emit('toglea-estado');
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
